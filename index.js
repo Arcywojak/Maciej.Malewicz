ae59e608b38a705e2eec6173964ba193ca31bca6
Arcywojak <maciej778@op.pl>
2020-06-03 14:18:55 +0200

    improve animation of dots and init second section

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const canvas = document.getElementById("canvas"),
+      speedInputX = document.getElementById("speedX"),
+      speedInputY = document.getElementById("speedY"),
+      numberOfDots = document.getElementById("nrDots"),     
       deviceWidth = window.innerWidth,
       deviceHeight = window.innerHeight;
-
-      const middleX = deviceWidth/2;
-      const middleY = deviceHeight/2;
-      // we need to minus half of height/width of the element to get the middle
+      middleX = deviceWidth/2,
+      middleY = deviceHeight/2,
+      ctx = canvas.getContext("2d");
 
 const resizeCanvas = () => {
     canvas.width = deviceWidth;
@@ -14,12 +16,21 @@ const resizeCanvas = () => {
 const generateRandomDots = (radius) => {
     const dots = [];
 
-    for(let i=0; i<100; i++){
+    let direction;
+
+    const dotsNumber = !isNaN(Number(numberOfDots.value)) ? Number(numberOfDots.value) : 5000
+
+    for(let i=0; i<dotsNumber; i++){
+
+        direction = i % 2 == 0 ? 1 : -1;
+
+        const positionInTime = Math.round(Math.random()*10000 + 1);
 
         const dot = {
             "radius":radius,
-            "x": Math.round(Math.random()*deviceWidth)+1,
-            "y": Math.round(Math.random()*deviceHeight)+1
+            "positionInTime": positionInTime,
+            "distanceFromCentre": Math.abs(Math.round(Math.random()*deviceWidth)+1),
+            "direction": direction
         }
 
         dots.push(dot);
@@ -28,31 +39,58 @@ const generateRandomDots = (radius) => {
     return dots;
 }
 
-const ctx = canvas.getContext("2d");
-
 const draw = () => {
 
-    const arrayOfDots = generateRandomDots(3)
+    let arrayOfDots = generateRandomDots(1)
 
-    for(let i=0; i<arrayOfDots.length; i++){
+    let counter = 1;
+
+    setInterval( () => {
+
+        if(Number(numberOfDots.value) !== arrayOfDots.length &&
+         !isNaN(Number(numberOfDots.value) ) 
+         && Number(numberOfDots.value) > 0){
+           
+            arrayOfDots = generateRandomDots(1);
+        }
+       
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+
+        for(let i=0; i<arrayOfDots.length; i++){
         
-        ctx.beginPath();
-        ctx.arc(
-                arrayOfDots[i].x, 
-                arrayOfDots[i].y,
-                arrayOfDots[i].radius,
-                0,
-                2*Math.PI, false
-            )
+            ctx.beginPath();
 
-        ctx.fill()
-    }
+            ctx.fillStyle = "#fefefe";
 
-    let rotateRate = 20;
+            ctx.arc(
+                    arrayOfDots[i].distanceFromCentre * Math.sin(
+                        2*
+                        Math.PI*
+                        (arrayOfDots[i].positionInTime + counter)/
+                        speedInputX.value*
+                        arrayOfDots[i].direction
+                        ) + middleX, // x Pos
+                    arrayOfDots[i].distanceFromCentre * Math.cos(
+                        2*
+                        Math.PI*
+                        (arrayOfDots[i].positionInTime + counter)/
+                        speedInputY.value*
+                        arrayOfDots[i].direction
+                        ) + middleY, // y pos 
+                    arrayOfDots[i].radius,
+                    0,
+                    2*Math.PI, false
+                )
     
+            ctx.fill()
+        }
+        
+        counter++;
 
-alert("ZROBIC OBRACANIE")
-      
+    },2)
+
+     
 
 }
 
